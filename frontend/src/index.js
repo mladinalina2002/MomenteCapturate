0632303ed3d4591bec8c7903f673d6976121e47c
mladinalina2002 <alinacristinamladin@gmail.com>
2022-12-01 20:36:49 +0200

    Second commit

---
+++ frontend/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import  ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Router>
+        <App />
+    </Router>
+     );
+
+
+/*     import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();*/
