27db781a402609ca9eab1c7dcc1d79d56cf3569c
mladinalina2002 <47451625+mladinalina2002@users.noreply.github.com>
2022-12-01 21:35:13 +0200

    Update client.js

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: "qlgvu6aq",
+    projectId: "",
     dataset: 'production',
     apiVersion: '2022-12-01',
     useCdn: true,
-    token: "skFxCcHP3quaawYG61GhQcniAn49Bs3BMfqKMfaAajYy38RtEgQ8nYr93ktaM2i5BDrxd79ly0v7jRyvJbRwg9sxahFUu6aPEETN1Vo3P5msWRu6bSzJFAkncTzSAVJKB8DxePHptzvMQlYen8jlDge0aaKxcmN84he6nUpKpv0X1FiR5",
+    token: "",
 });
 
 const builder= imageUrlBuilder(client);
